e54e6f49245188ef5ea600236a9809eb29aec472
Sergey Kamskov <sk1999@inbox.ru>
2021-03-11 16:31:46 +0300

    iter3

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,15 +1,7 @@
 long long power(long long x, unsigned short n){
-<<<<<<< HEAD
-=======
-
->>>>>>> 397d45b8f152e72c34585a65aeb1d914b0b74831
     long long tmp = 1;
     for(int i = 0; i < n; i++){
         tmp *= x;
     }
     return tmp;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 397d45b8f152e72c34585a65aeb1d914b0b74831
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 #include "fun.h"
 #include <iostream>
-using namespace std;
-int main()
-{
-    cout << power(3,4)<< endl;
-    
-    return 0;
-}
-=======
-#include "..\include\fun.h"
 
-#include <iostream>
 using namespace std;
 
 int main()
@@ -19,4 +8,4 @@ int main()
     cout << power(3,4)<< endl;
     return 0;
 }
->>>>>>> 397d45b8f152e72c34585a65aeb1d914b0b74831
+
